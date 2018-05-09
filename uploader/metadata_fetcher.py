3ebf886399ee7c1a55c1255b24d8ba793ef9d3ca
Davide Allavena <davide.allavena@gmail.com>
2018-05-09 16:20:04 +0200

    Changed upload method

--- uploader/metadata_fetcher.py
+++ uploader/metadata_fetcher.py
@@ -11,8 +11,8 @@ import glob
 #import shutil
 
 if os.path.exists('./book.csv'):
-    doneBooks = pd.read_csv('book.csv', dtype=str, encoding='utf-8')
-    done = doneBooks['identifier'].tolist()
+    bookdf = pd.read_csv('book.csv', dtype=str, encoding='utf-8')
+    done = bookdf['identifier'].tolist()
 else:
     done = []
 
@@ -39,6 +39,7 @@ for directory in os.listdir(basedir):
             tree = ET.parse(filename)
             root = tree.getroot()
             identifier = element.replace(".xml", "")
+            print "Creating pdf for " + identifier
 
             for child in root.find(namespace+'bib'):
 
@@ -71,26 +72,23 @@ for directory in os.listdir(basedir):
 
 
                 bookObj['mediatype'] = 'texts'
-                #files = glob.glob(filename.replace(".xml","") + '/Internet/*')
-                #files = glob.glob(filename.replace(".xml","") + '/*.zip')
-            convert_code = os.system('convert "' + basedir + directory +  "/" + element.replace(".xml", "") + '/Archivio/*.tif" -quality 100 ' + identifier + '.pdf')
 
-            print bookObj['identifier']
-
-            if convert_code != 0:
+            zip_exit_code = os.system('zip -r ' + identifier + '_images.zip ' + basedir + directory.replace(' ', '\ ') +  '/' + element.replace(".xml", "") + '/Archivio/*')
+            #sys.exit(0)
+            if zip_exit_code != 0:
 
                 print "ERROR in creating the pdf. Exit code is: " + str(convert_code)
-                system.exit(convert_code)
+                sys.exit(convert_code)
 
             print "Uploading"
             #print bookObj
-            r = upload(bookObj['identifier'],files=identifier + '.pdf', metadata=bookObj)
+            r = upload(bookObj['identifier'],files=identifier + '_images.zip', metadata=bookObj)
 
             code = r[0].status_code
 
             if code != 200:
                 print "ERROR uploading with code: " + str(code)
-                system.exit(code)
+                sys.exit(code)
 
             print "Done"
             bookObj['resCode'] = code
@@ -99,10 +97,9 @@ for directory in os.listdir(basedir):
             #print bookObj['identifier']
             #sys.exit(0)
             data.append(bookObj)
-            bookdf = pd.DataFrame(data)
+            thisbookdf = pd.DataFrame(data)
+            bookdf = bookdf.append(thisbookdf, ignore_index=True)
             bookdf.to_csv('book.csv', index=False, encoding='utf-8')
-
-
 
 """
 {http://purl.org/dc/elements/1.1/}publisher
