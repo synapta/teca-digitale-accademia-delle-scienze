50880e7fac9eeba4955aa4d0373175731dd3efc9
Davide Allavena <davide.allavena@gmail.com>
2018-05-09 13:15:57 +0200

    Uploader changed to read hd files

--- uploader/metadata_fetcher.py
+++ uploader/metadata_fetcher.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import pandas as pd
 import os
 import xml.etree.ElementTree as ET
@@ -6,58 +8,99 @@ from numpy import nan as Nan
 from internetarchive import upload
 import sys
 import glob
+#import shutil
 
-basedir = "/data/davide/accademia/teca/storage/Asto"
+if os.path.exists('./book.csv'):
+    doneBooks = pd.read_csv('book.csv', dtype=str, encoding='utf-8')
+    done = doneBooks['identifier'].tolist()
+else:
+    done = []
+
+basedir = "/media/davide/Volume/ASTO/"
 namespace = '{http://www.iccu.sbn.it/metaAG1.pdf}'
 purl = '{http://purl.org/dc/elements/1.1/}'
 
-data = pd.DataFrame()
+data = []
 #columns=['date', 'date0', 'coverage', 'creator', 'coverage', 'credits', 'description', 'language', 'licenseurl', 'mediatype'])
 
 
 for directory in os.listdir(basedir):
     print directory
     for element in os.listdir(basedir + '/' + directory):
-        if ".xml" == element[-4:]:
+        if ".xml" == element[-4:] and element.replace(".xml", "") not in done:
+            #and 'TO01157391_TO0324_62137_000000' in element:
+            #print 'uploading'
 
             bookObj = {}
+            part_name = ""
+
             filename = basedir + '/' + directory + '/' +  element
+
             tree = ET.parse(filename)
             root = tree.getroot()
+            identifier = element.replace(".xml", "")
 
             for child in root.find(namespace+'bib'):
-                if child.tag == purl+'identifier':
+
+                if 'identifier' in child.tag:
                     bookObj['identifier'] = element.replace(".xml", "")
-                elif child.tag == purl+'title':
+                elif 'title' in child.tag:
                     bookObj['title'] = child.text
-                elif child.tag == purl+'creator':
+                elif 'creator'in child.tag:
                     bookObj['creator'] = child.text
-                elif child.tag == purl+'publisher':
+                elif 'publisher' in child.tag:
                     bookObj['publisher'] = child.text
-                elif child.tag == purl+'description':
+                elif 'description' in child.tag:
                     bookObj['description'] = child.text
-                elif child.tag == purl+'date':
+                elif 'date' in child.tag:
                     if 'date' not in bookObj.keys():
-                        bookObj['date[0]'] = child.text
-                    else:
-                        bookObj['date[1]'] = child.text
-                elif child.tag == purl+'format':
-                    bookObj['format'] = child.text
-                elif child.tag == purl+'language':
+                        bookObj['date'] = child.text
+                #elif 'format' in child.tag:
+                #    bookObj['format'] = child.text
+                elif 'language' in child.tag:
                     bookObj['language'] = child.text
+                elif 'piece' in child.tag:
+                    if child.find(namespace + 'part_name') is not None:
+                        part_name = child.find(namespace + 'part_name').text
+                    elif child.find(namespace + 'issue') is not None:
+                        #print child.find(namespace + 'issue').text
+                        part_name = child.find(namespace + 'issue').text
+
+                if part_name != "":
+                    bookObj['title'] = bookObj['title'] + " " + part_name
+
+
                 bookObj['mediatype'] = 'texts'
-                files = glob.glob(filename.replace(".xml","") + '/Internet/*')
-                print files, bookObj
-                #r = upload(bookObj['identifier'],files=files, metadata=bookObj)
-                #print r[0].status_code
-                sys.exit(0)
-        break
-    break
+                #files = glob.glob(filename.replace(".xml","") + '/Internet/*')
+                #files = glob.glob(filename.replace(".xml","") + '/*.zip')
+            convert_code = os.system('convert "' + basedir + directory +  "/" + element.replace(".xml", "") + '/Archivio/*.tif" -quality 100 ' + identifier + '.pdf')
 
-#        bookDf = pd.DataFrame(bookObj)
-#        data = data.append(bookDf)
+            print bookObj['identifier']
 
-#data.to_csv('book.csv',encoding='utf8', index=False)
+            if convert_code != 0:
+
+                print "ERROR in creating the pdf. Exit code is: " + str(convert_code)
+                system.exit(convert_code)
+
+            print "Uploading"
+            #print bookObj
+            r = upload(bookObj['identifier'],files=identifier + '.pdf', metadata=bookObj)
+
+            code = r[0].status_code
+
+            if code != 200:
+                print "ERROR uploading with code: " + str(code)
+                system.exit(code)
+
+            print "Done"
+            bookObj['resCode'] = code
+
+            #shutil.copytree(filename.replace(".xml","") + '/Internet/', filename.replace(".xml","") + /bookObj['identifier'])
+            #print bookObj['identifier']
+            #sys.exit(0)
+            data.append(bookObj)
+            bookdf = pd.DataFrame(data)
+            bookdf.to_csv('book.csv', index=False, encoding='utf-8')
 
 
 
@@ -78,4 +121,12 @@ for directory in os.listdir(basedir):
 {http://purl.org/dc/elements/1.1/}format
 {http://purl.org/dc/elements/1.1/}language
 {http://www.iccu.sbn.it/metaAG1.pdf}holdings
+
+
+ * Part 1 | THIS WORLD | 1
+ ** Chapter 1 | Of the Nature of Flatland | 3
+ ** Chapter 2 | Of the Climate and Houses in Flatland | 5
+ * Part 2 | OTHER WORLDS | 42
+
+
 """
