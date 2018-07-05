535b60a7baafd0b858a514b04d6133f50068b8b7
Davide Allavena <davide.allavena@gmail.com>
2018-07-05 15:39:18 +0200

    Bugfix

--- reload_books.sh
+++ reload_books.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cd /root/public/data/ && curl 'https://docs.google.com/spreadsheets/d/13u2k8_MVktUBamh-UZec3ylDnXQfQIp7HZT8AgB0MOU/export?format=csv&id=13u2k8_MVktUBamh-UZec3ylDnXQfQIp7HZT8AgB0MOU&gid=715455760' > book.csv && csvtojson book.csv > book.json &&  if [ $1 != 'first' ] ; then kill 1 ; fi
+cd /root/public/data/ && curl 'https://docs.google.com/spreadsheets/d/13u2k8_MVktUBamh-UZec3ylDnXQfQIp7HZT8AgB0MOU/export?format=csv&id=13u2k8_MVktUBamh-UZec3ylDnXQfQIp7HZT8AgB0MOU&gid=715455760' > book.csv && csvtojson book.csv > book.json &&  if [ -z $1 ] ; then kill 1 ; fi
